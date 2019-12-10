f7e42d0c538ea402506a2a869e3c87f392ec12e2
Iñaki Arango <arangoinaki@gmail.com>
2019-12-09 22:08:57 -0300

    Switched IPFS endpoint to Docsit Gateway

--- src/ipfs.js
+++ src/ipfs.js
@@ -6,12 +6,12 @@ import ipfsClient from 'ipfs-http-client';
  */
 
 const createIpfsClient = () =>
-  // ipfsClient({
-  //   host: '165.22.36.48',
-  //   protocol: 'http',
-  //   port: 5001,
-  //   apiPath: '/api/v0',
-  // });
-  ipfsClient('/ip4/127.0.0.1/tcp/5001');
+  ipfsClient({
+    host: '165.22.36.48',
+    protocol: 'http',
+    port: 5001,
+    apiPath: '/api/v0',
+  });
+  // ipfsClient('/ip4/127.0.0.1/tcp/5001');
 
 export default createIpfsClient;
